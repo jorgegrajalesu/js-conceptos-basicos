1ee7cbf18af71f052566c123b92aa10e111704dc
jorgegrajalesu <jegosystem@gmail.com>
2023-07-11 18:52:18 -0500

    variables

---
+++ js/variables.js
@@ -0,0 +1,15 @@
+// para declarar variables se utilizan var, let y const
+/**
+ * tipos datos son: string (textos), number, boolean(false, true),
+ * symbol, object, null y undefined
+ */
+
+let edad = 20;// tipo number
+
+const miNombre = 'Jorge'; // tipo string
+
+const casado = false; // tipo boolean
+
+edad = 96;
+
+console.log(edad);
