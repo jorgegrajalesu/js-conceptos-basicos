072f2b8bf4b95f18f606bd5802f6f728f7f1c96d
jorgegrajalesu <jegosystem@gmail.com>
2023-07-11 19:16:22 -0500

    110723

--- js/variables.js
+++ js/variables.js
@@ -18,6 +18,23 @@ console.table('Mi edad es: ' + edad, 'Casado: ' + casado, 'Mi nombre ' + miNombr
 
 const num1 = 20;
 const num2 = 20;
-const suma = num1 + num2;
+const num3 = 60;
+const suma = num1 + num2 + num3;
 
 console.log(`La suma es: ${suma}`);//concatenacion con block tic
+
+// resta
+const num_1 = 20;
+const num_2 = 20;
+const num_3 = 60;
+const resta = num_1 - num_2 - num_3;
+
+console.log(`La resta es: ${resta}`);//concatenacion con block tic
+
+// producto
+const Num_1 = 20;
+const Num_2 = 20;
+const Num_3 = 60;
+const prod = Num_1 * Num_2 * Num_3;
+
+console.log(`La resta es: ${prod}`);//concatenacion con block tic
