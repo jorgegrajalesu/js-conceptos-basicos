e90fc9db83066c0bd68d6b877d309643343f7dc9
jorgegrajalesu <jegosystem@gmail.com>
2023-07-13 20:09:01 -0500

    funciones130723

---
+++ js/funciones.js
@@ -0,0 +1,20 @@
+// funciones
+/**
+ * funciones tradicional function
+ */
+
+function primerNombre(){
+    console.log('Jorge');
+}
+
+// llamar funcion
+primerNombre();
+
+// otra forma de crear funciones, función anonima
+const segundoNombre = function(){
+    console.log('Enrique');
+}
+
+
+
+
