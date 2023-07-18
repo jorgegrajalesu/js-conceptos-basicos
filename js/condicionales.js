fd0c9b2d303104b5385d470fdc484922fe5eeb2f
jorgegrajalesu <jegosystem@gmail.com>
2023-07-18 18:29:39 -0500

    180723

--- js/condicionales.js
+++ js/condicionales.js
@@ -1,12 +1,12 @@
 // if y else 
 
 // ejemplo1: Las personas mayores igual a 18 pueden ingresar a un bar
-let edad = 0;
+let edad = 17;
 
 if (edad >= 18) {
 
     console.log(`Puedes ingrear, tu edad es ${edad}`);
     
 }else{
-    console.log('No puedes ingresar');
+    console.log('No puedes ingresar, tu edad es: ' + edad);
 }
